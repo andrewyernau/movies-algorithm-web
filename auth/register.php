a96ff8d3a0e84d44b6af7f2e73a1a131a6190f8d
varo6 <103251919+varo6@users.noreply.github.com>
2025-01-03 14:21:57 +0100

    edit auth css

--- auth/register.php
+++ auth/register.php
@@ -1,46 +1,44 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    echo file_get_contents('registerform.html'); 
-    exit;
+if ($_SERVER["REQUEST_METHOD"] === "GET") {
+    echo file_get_contents("registerform.html");
+    exit();
 }
 
-function initialize_params($post_data, $pdo) {
-    $params = array();
+function initialize_params($post_data, $pdo)
+{
+    $params = [];
     $query = "SELECT MAX(id) FROM users";
     $result = $pdo->query($query);
     $params[0] = $result->fetchColumn() + 1;
-    $params[1] = htmlspecialchars($post_data['name']);
-    $params[2] = isset($post_data['edad']) ? intval($post_data['edad']) : null;
-    $params[3] = htmlspecialchars($post_data['sex'] ?? '');
-    $params[4] = htmlspecialchars($post_data['ocupacion'] ?? '');
-    $params[5] = htmlspecialchars($post_data['pic'] ?? '');    
-    $params[6] = sha1($post_data['password']);
+    $params[1] = htmlspecialchars($post_data["name"]);
+    $params[2] = isset($post_data["edad"]) ? intval($post_data["edad"]) : null;
+    $params[3] = htmlspecialchars($post_data["sex"] ?? "");
+    $params[4] = htmlspecialchars($post_data["ocupacion"] ?? "");
+    $params[5] = htmlspecialchars($post_data["pic"] ?? "");
+    $params[6] = sha1($post_data["password"]);
     return $params;
 }
 
-require_once '../includes/conectar.php';
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+require_once "../includes/conectar.php";
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
     try {
         $pdo = conectar();
-   
+
         $params = initialize_params($_POST, $pdo);
 
         // Creamos una fila en la tabla.
         $query = "INSERT INTO users (id, name, edad, sex, ocupacion, pic, passwd ) VALUES ($params[0], '$params[1]', $params[2], '$params[3]', '$params[4]', '$params[5]', '$params[6]')";
         $pdo->query($query);
 
-        
-        $message = '<div style="text-align: center; color: green; margin-bottom: 20px;">¡Registrado! Ahora inicia sesión</div>';
-        $button = '<div style="text-align: center;"><a href="../index.html" style="text-decoration: none; color: #000; background-color: #f1f1f1; padding: 10px 20px; border-radius: 5px;">Iniciar sesión</a></div>';
+        $message =
+            '<div style="text-align: center; color: green; margin-bottom: 20px;">¡Registrado! Ahora inicia sesión</div>';
+        $button =
+            '<div style="text-align: center;"><a href="../index.html" style="text-decoration: none; color: black; background-color: #f1f1f1; padding: 10px 20px; border-radius: 5px;">Iniciar sesión</a></div>';
         echo $message;
         echo $button;
-        
     } catch (PDOException $e) {
         echo "Error: " . $e->getMessage();
     }
-
-
-
 }
 ?>
