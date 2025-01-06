b9cd87b13c458c5180cc658976e35d4ea29e69d8
André Yermak Naumenko <asyermak@gmail.com>
2025-01-06 10:33:38 +0100

    Recomendacion

--- matlab/getData.m
+++ matlab/getData.m
@@ -1,86 +1,62 @@
-function [Y, R, movieList] =  getData()
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+function [Y, R, movieList] = getData()
+    import java.sql.Connection;
+    import java.sql.DriverManager;
+    import java.sql.ResultSet;
+    import java.sql.SQLException;
+    import java.sql.Statement;
 
-%Ponga el nombre de su base de datos al final
-url = 'jdbc:mysql://localhost:3306/ai0';
-%Ponga su usuario aqui
-user = 'ai0';
-password = '';
+    % Configuración de la conexión
+    url = 'jdbc:mysql://localhost:3306/ai0';
+    user = 'ai0';
+    password = '';
 
-con = DriverManager.getConnection(url, user, password);
-%Cargamos las peliculas
-st = con.createStatement();
-rs = st.executeQuery('SELECT COUNT(id) FROM movie');
-rs.next();
-nmovies = rs.getInt(1);
+    con = DriverManager.getConnection(url, user, password);
 
-rs = st.executeQuery('SELECT COUNT(id) FROM users');
-rs.next();
-nusers = rs.getInt(1);
+    % Obtenemos el número de películas y usuarios
+    st = con.createStatement();
+    rs = st.executeQuery('SELECT COUNT(id) FROM movie');
+    rs.next();
+    num_movies = rs.getInt(1);
 
-rs = st.executeQuery('SELECT COUNT(id) FROM movie');
-rs.next();
-nmovies = rs.getInt(1);
+    rs = st.executeQuery('SELECT COUNT(id) FROM users');
+    rs.next();
+    num_users = rs.getInt(1);
 
-rs = st.executeQuery('SELECT COUNT(id) FROM movie');
-rs.next();
-nmovies = rs.getInt(1);
+    try rs.close(); catch, end
+    try st.close(); catch, end
 
-movieList = cell(1, 1);
-i=1;
-while rs.next()
-    movieList{i}=char(rs.getString(1));
-    i=i+1;
-end
-num_movies=i-1;
+    % Obtenemos la lista de películas
+    st = con.createStatement();
+    rs = st.executeQuery('SELECT title FROM movie ORDER BY id');
+    
+    movieList = cell(num_movies, 1);
+    i = 1;
+    while rs.next()
+        movieList{i} = char(rs.getString(1));
+        i = i + 1;
+    end
 
-try rs.close(); catch, end
+    try rs.close(); catch, end
+    try st.close(); catch, end
 
-try st.close(); catch, end
+    % Inicializamos las matrices Y y R
+    Y = zeros(num_movies, num_users);
+    R = zeros(num_movies, num_users);
 
-%Creamos la matrix R vacia, necesitamos saber el numero de usuarios
+    % Obtenemos las puntuaciones y construimos las matrices Y y R
+    st = con.createStatement();
+    rs = st.executeQuery('SELECT id_user, id_movie, score FROM user_score');
 
-st = con.createStatement();
-rs = st.executeQuery('SELECT COUNT(id) FROM users');
+    while rs.next()
+        movie_id = rs.getInt(2);
+        user_id = rs.getInt(1);
+        Y(movie_id, user_id) = rs.getInt(3);
+        R(movie_id, user_id) = 1;
+    end
 
-rs.next();
-num_users= rs.getInt(1);
-
-
-try rs.close(); catch, end
-
-try st.close(); catch, end
-R=zeros(num_movies,num_users);
-
-
-
-
-
-
-%Construimos la matriz Y y la matrix R
-
-Y=zeros(num_movies, num_users);
-st = con.createStatement();
-rs = st.executeQuery('SELECT id_user, id_movie, score FROM user_score');
-
-while rs.next()
-    movid=rs.getInt(2);
-    usid=rs.getInt(1);
-    Y(movid,usid)=rs.getInt(3);
-    R(movid,usid)=1;
-end
-
-
-try rs.close(); catch, end
-
-try st.close(); catch, end
-
-%Cerramos
-
-try con.closeAllStatements(); catch, end
-try con.close(); catch, end
+    % Cerramos las conexiones
+    try rs.close(); catch, end
+    try st.close(); catch, end
+    try con.closeAllStatements(); catch, end
+    try con.close(); catch, end
 end
--- matlab/main.m
+++ matlab/main.m
@@ -1,12 +1,15 @@
+% Cargamos los datos de la base de datos
 [Y, R, movieList] = getData();
-num_movies = size(Y, 1);
-pi = zeros(num_movies, 1);
+[num_movies, num_users] = size(Y);
 
+%% puntuaciones Bayesianas
+pi = zeros(num_movies, 1);
 N = num_movies;
 total_scores = sum(Y(:));
 total_ratings = sum(R(:));
 R_global = total_scores / total_ratings;
 
+% Calculamos la puntuación Bayesiana para cada película
 for i = 1:num_movies
     ni = sum(R(i, :));
     if ni > 0
@@ -16,6 +19,42 @@ for i = 1:num_movies
     end
     pi(i) = puntuacionBayesian(ni, ri, N, R_global);
 end
+
+% Actualizamos las puntuaciones en la base de datos
 updateRatings(pi, movieList);
 
+%%recomendación
 
+% Normalizamos las puntuaciones
+[Ynorm, Ymean] = normalizeRatings(Y, R);
+
+% Parámetros para el sistema de recomendación
+num_features = 10;
+lambda = 0.1;
+
+% Inicializamos las matrices de características aleatoriamente
+X = randn(num_movies, num_features);
+Theta = randn(num_users, num_features);
+
+% Minimizamos la función de coste usando fmincg
+params = [X(:); Theta(:)];
+costFunction = @(p) cofiCostFunc(p, Ynorm, R, num_users, num_movies, num_features, lambda);
+options = optimset('MaxIter', 100);
+[theta, cost] = fmincg(costFunction, params, options);
+
+% Recuperamos las matrices optimizadas
+X = reshape(theta(1:num_movies*num_features), num_movies, num_features);
+Theta = reshape(theta(num_movies*num_features+1:end), num_users, num_features);
+
+% Generamos predicciones para todos los usuarios
+predictions = X * Theta';
+
+% Desnormalizamos
+predictions = predictions + repmat(Ymean, 1, num_users);
+
+% Actualizamos las recomendaciones para cada usuario
+for user_id = 1:num_users
+    my_predictions = predictions(:, user_id);
+    num_updated = updateRecommendation(my_predictions, user_id);
+    fprintf('Actualizadas %d recomendaciones para el usuario %d\n', sum(num_updated), user_id);
+end
